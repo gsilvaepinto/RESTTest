0df4ce6070e38ebce3bc458d7540201d7896bab4
Guilherme Pinto <109026846+gsilvaepinto@users.noreply.github.com>
2025-06-09 21:23:24 +0100

    Add files via upload

--- router/comments.js
+++ router/comments.js
@@ -1,24 +1,58 @@
 const express = require('express');
 const router = express.Router();
+const { v4: uuid } = require('uuid');
 
 const comments = [
-    { username: 'Todd', comment: 'lol that is so funny!' },
-    { username: 'Skyler', comment: 'I like to go shopping' },
-    { username: 'Rita', comment: 'pls delete your account, todd' }
-];
+    {id: uuid(), username: 'Todd', comment: 'example1'},
+    {id: uuid(), username: 'Maria', comment: 'example2'},
+    {id: uuid(), username: 'John', comment: 'example3'},
+]
 
 router.route('/')
     .get((req, res) => {
-        res.render('comments', {comments});
+        res.render('comments/comments', {comments});
     })
     .post((req, res) => {
         const {username, comment} = req.body;
-        comments.push({username, comment});
+        comments.push({username, comment, id: uuid() });
         res.redirect('/comments');
     })
 
-router.get('/new', (req, res) => {
-    res.render('new');
-})
+router.route('/compose')
+    .get((req, res) => {
+        res.render('comments/compose');
+    })
+
+router.route('/:id')
+    .patch((req, res) => {
+        const { id } = req.params;
+        const newCommentText = req.body.comment;
+        const foundComment = comments.find(c => c.id === id);
+        if (!foundComment) {
+            return res.status(404).send('Comment not found');
+        }
+        foundComment.comment = newCommentText;
+        res.redirect('/comments');
+    });
+
+router.route('/:id/edit')
+    .get((req, res) => {
+        const { id } = req.params;
+        const comment = comments.find(c => c.id === id);
+        if (!comment) {
+            return res.status(404).send('Comment not found');
+        }
+        res.render('comments/edit', { comment }); 
+    });
+
+router.route('/:id')
+    .get((req, res) => {
+        const {id} = req.params;
+        const comment = comments.find(c => c.id === id);
+        if (!comment){
+            return res.status(404).send('NO COMMENT FOUND');
+        }
+        res.render('comments/show', {comment});
+    })
 
 module.exports = router;
